ed698e75b034143c7543aa1a5f6ae45cd874c885
karthikgoud <karthikrv09@gmail.com>
2022-05-05 19:22:17 +0530

    Final app done

--- src/Form.js
+++ src/Form.js
@@ -19,25 +19,32 @@ class Form extends Component {
         )
     }
 
+    submitForm = () => {
+        this.props.handleSubmit(this.state)
+        this.setState(this.initialState)
+    }
+
     render() {
         const { name,job } = this.state
 
         return (
             <form>
-                <label>Name</label>
+                <label for="name">Name</label>
                 <input
                     type='text'
                     name = 'name'
                     id = 'name'
                     value = {name}
                     onChange = {this.handleChange} />  
-                <label htmlFor="job">Job</label>
+                <label for="job">Job</label>
                 <input
                     type="text"
                     name="job"
                     id="job"
                     value={job}
-                    onChange={this.handleChange} />   
+                    onChange={this.handleChange} />  
+                    
+                <input type="button" value="Submit" onClick={this.submitForm} /> 
             </form>
         )
     }
